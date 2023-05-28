95e049d1013cd211744f906c68daf17e66515f57
ivan villahermosa <villahermosaivan82@gmail.com>
2023-05-28 11:47:52 +0800

    First db.query postgresql to test connection

--- app/main.py
+++ app/main.py
@@ -1,7 +1,15 @@
-from fastapi import Body, FastAPI, HTTPException, status
+from fastapi import Body, FastAPI, HTTPException, status, Depends
 from pydantic import BaseModel
 import psycopg2
 from psycopg2.extras import RealDictCursor
+import time
+from . import models
+from .database import engine, get_db
+from sqlalchemy.orm import Session
+
+
+# this will create the tables in postgress
+models.Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
 
@@ -11,14 +19,17 @@ class Post(BaseModel):
     content: str
 
 
-try:
-    conn = psycopg2.connect(host='localhost', database='fastapi', port=54321,
-                            user='postgres', password='sa', cursor_factory=RealDictCursor)
-    cursor = conn.cursor
-    print("Database connection was successfull!")
-except Exception as error:
-    print("Connecting to database failed!!!")
-    print("Error: ", error)
+while True:
+    try:
+        conn = psycopg2.connect(host='localhost', database='fastapi', port=54321,
+                                user='postgres', password='sa', cursor_factory=RealDictCursor)
+        cursor = conn.cursor()
+        print("Database connection was successfull!")
+        break
+    except Exception as error:
+        print("Connecting to database failed!!!")
+        print("Error: ", error)
+        time.sleep(2)
 
 my_posts = [{"title": "title of post 1",
              "content": "content of post 1", "id": 1},
@@ -33,8 +44,11 @@ def find_post(id):
 
 
 @app.get("/posts")
-def get_posts():
-    return {"data": "This is your post!!!"}
+def get_posts(db: Session = Depends(get_db)):
+    # cursor.execute("""SELECT * FROM posts """)
+    # posts = cursor.fetchall()
+    posts = db.query(models.Post).all()
+    return {"data": posts}
 
 
 # add status code in parameter for return
@@ -46,8 +60,15 @@ def create_posts(post: Post):
 # use HttpException from now on
 @app.get("/posts/{id}")
 def get_post(id: int):
-    post = find_post(id)
+    cursor.execute("""SELECT * FROM posts WHERE id = %s""", (str(id)))
+    post = cursor.fetchone()
     if not post:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
                             detail=f"post with id: {id} was not found")
     return {"post_detail": post}
+
+
+@app.get('/sqlalchemy')
+def test_posts(db: Session = Depends(get_db)):
+    posts = db.query(models.Post).all()
+    return {"data": posts}
