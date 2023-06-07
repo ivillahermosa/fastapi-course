5b1b49d87c3c0c721d94e7a1d2e806057be68b29
ivan villahermosa <villahermosaivan82@gmail.com>
2023-06-07 23:15:34 +0800

    Update table user to check postgrsql render update

---
+++ alembic/versions/47951a6e1e70_updated_user_table_for_testing_render.py
@@ -0,0 +1,24 @@
+"""updated user table for testing render
+
+Revision ID: 47951a6e1e70
+Revises: 2af06ec8685d
+Create Date: 2023-06-07 23:03:36.756478
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '47951a6e1e70'
+down_revision = '2af06ec8685d'
+branch_labels = None
+depends_on = None
+
+
+def upgrade() -> None:
+    pass
+
+
+def downgrade() -> None:
+    pass
